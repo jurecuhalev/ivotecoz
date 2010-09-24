4d06ad24311c34afebc784471fd131d5e1eb289d
Jure Cuhalev <gandalfar@gmail.com>
2010-09-24 19:07:03 +0200

    skeleton

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='ivotecoz',
+      version=version,
+      description="Election promotion site",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Jure',
+      author_email='g@owca.info',
+      url='',
+      license='APACHE',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
